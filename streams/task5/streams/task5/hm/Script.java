a9bf89660d49fa70eee69ca0e25a7b72e6768224
Emilio Stien <emilio.stien@etu.univ-grenoble-alpes.fr>
2025-01-04 14:41:00 +0100

    modifs step2 ok mais pas step3

---
+++ streams/task5/streams/task5/hm/Script.java
@@ -0,0 +1,28 @@
+package streams.task5.hm;
+import oop.streams.InputStream;
+import oop.streams.OutputStream;
+
+public class Script {
+  /*
+   * words: the array of secret words.
+   * is: the input stream that provides the player's moves.
+   * os: the output stream to print to.
+   */
+  public Script(String words[], InputStream is, OutputStream os) {
+	  
+  }
+
+  /*
+   * Plays the script.
+   */
+  public void play() {
+	  
+  }
+
+  /*
+   * Give access to the instance of our game.
+   */
+  public HangedMan game() {
+	  
+  }
+}
