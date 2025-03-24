7a7f6dc52eaed017a7e3a9639dea9bea6053c2c9
Emilio Stien <emilio.stien@etu.univ-grenoble-alpes.fr>
2025-03-24 12:03:46 +0100

    Sprint collections task2, step 3 ok + modifs tests

--- collections/task2/oop/utils/collections/LinkedList.java
+++ collections/task2/oop/utils/collections/LinkedList.java
@@ -4,104 +4,216 @@ import oop.collections.ICollection;
 import oop.collections.IList;
 
 public class LinkedList implements IList {
-	
-	public static class Iterator implements ICollection.Iterator{
-		
-		public Iterator(LinkedList m_list) {
-			
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return false;
-		}
-		
-		@Override
-		public Object next() {
-			return 0;
+
+	private static class Node {
+
+		Object value;
+		Node next;
+
+		Node(Object value) {
+			this.value = value;
+			this.next = null;
 		}
 	}
+
+	public static class Iterator implements ICollection.Iterator {
+
+		private Node cell;
+
+		public Iterator(LinkedList m_list) {
+			this.cell = m_list.head;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return cell != null;
+		}
+
+		@Override
+		public Object next() {
+			if (!hasNext()) {
+				throw new IndexOutOfBoundsException("No more elements");
+			}
+			Object elem = cell.value;
+			cell = cell.next;
+			return elem;
+		}
+	}
+
+	// Fields
+	private Node head;
+	private int size;
 
 	/**
 	 * Constructs an empty list.
 	 */
 	public LinkedList() {
-
+		this.head = null;
+		this.size = 0;
 	}
 
 	/**
 	 * Constructs a list, initialized with the elements from the given array.
 	 */
 	public LinkedList(Object array[]) {
-
+		for (Object obj : array) {
+			insertAt(size, obj);
+		}
 	}
 
 	/**
 	 * Constructs a list, initialized with the elements from the given list.
 	 */
 	public LinkedList(LinkedList v) {
-
+		Node cell = v.head;
+		while (cell != null) {
+			insertAt(size, cell.value);
+			cell = cell.next;
+		}
 	}
 
 	/**
 	 * Constructs a list, initialized with the elements from the given collection.
 	 */
 	public LinkedList(ICollection c) {
-
+		Object[] m_obj = new Object[c.length()];
+		c.toArray(m_obj);
+		for (Object obj : m_obj) {
+			insertAt(size, obj);
+		}
 	}
 
 	@Override
 	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Iterator(this);
 	}
 
 	@Override
 	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public Object elementAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid index");
+		}
+		Node cell = head;
+		for (int i = 0; i < index; i++) {
+			cell = cell.next;
+		}
+		return cell.value;
+
 	}
 
 	@Override
 	public Object updateAt(int index, Object niu) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid Index");
+		}
+		Node cell = head;
+		for (int i = 0; i < index; i++) {
+			cell = cell.next;
+		}
+		Object elem = cell.value;
+		elem = niu;
+		cell.value = elem;
+		return elem;
 	}
 
 	@Override
 	public void insertAt(int index, Object elem) {
-		// TODO Auto-generated method stub
+		if (index < 0) {
+			throw new IndexOutOfBoundsException("Invalid Index");
+		}
+
+		// Cas où index > size
+		while (size < index) {
+			insertAt(size, null);
+		}
+
+		Node cell = new Node(elem);
+		if (index == 0) {
+			cell.next = head;
+			head = cell;
+		} else {
+			Node current = head;
+			for (int i = 0; i < index - 1; i++) {
+				// on se place à la position où on veut insérer
+				current = current.next;
+			}
+			cell.next = current.next;
+			current.next = cell;
+		}
+		size++;
 
 	}
 
 	@Override
 	public Object removeAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid Index");
+		}
+		
+		Object obj_suppr;
+		if (index == 0) {
+			obj_suppr = head.value;
+			head = head.next;
+		} else {
+			Node cell = head;
+			for (int i = 0; i < index - 1; i++) {
+				cell = cell.next;
+			}
+			Node toRemove = cell.next;
+			obj_suppr = toRemove.value;
+			cell.next = toRemove.next;
+		
+		}
+		size--;
+		return obj_suppr;
 	}
 
 	@Override
 	public boolean remove(Object elem) {
-		// TODO Auto-generated method stub
+		if (head == null) {
+			return false;
+		}
+		if (head.value.equals(elem)) {
+			head = head.next;
+			size--;
+			return true;
+		}
+		Node cell = head;
+		while (cell.next != null) {
+			if (cell.next.value.equals(elem)) {
+				cell.next = cell.next.next;
+				size--;
+				return true;
+			}
+			cell = cell.next;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean contains(Object elem) {
-		// TODO Auto-generated method stub
+		Node cell = head;
+		while (cell != null) {
+			if (cell.value.equals(elem)) {
+				return true;
+			}
+			cell = cell.next;
+		}
 		return false;
 	}
 
 	@Override
 	public void toArray(Object[] elems) {
-		// TODO Auto-generated method stub
-
+		head = null;
+		size = 0;
+		for (Object obj : elems) {
+			insertAt(size, obj);
+		}
 	}
 
 }
--- collections/tests/oop/collections/tests/AllTests.java
+++ collections/tests/oop/collections/tests/AllTests.java
@@ -1,165 +1,201 @@
 package oop.collections.tests;
 
+import oop.collections.ICollection;
 import oop.collections.IList;
 import oop.utils.collections.ArrayList;
-
+import oop.utils.collections.LinkedList;
 
 public class AllTests {
+
+	private static int nbTestsPassed = 0;
+	private static boolean passed = false;
 
 	public static void main(String args[]) {
 
 		// TESTS ArrayList
-		ArrayListTests.execTests();
-	}
-
-	public static class ArrayListTests {
-		static boolean test01() {
-			boolean passed = true;
-			Object[] m_obj = { 1, 2, 3, 4, 5 };
-			IList list = new ArrayList(m_obj);
-			if (list.length() != 5) {
-				System.out.println("Wrong size ! \n");
-				passed = false;
-			}
-			for (int i = 0; i < list.length(); i++) {
-				if (!list.elementAt(i).equals(m_obj[i])) {
-					System.out.println("Wrong element ! \n");
-					passed = false;
-				}
-			}
-			
-			iteratorTest(list);
-			
-			if (!passed) {
-				System.out.println("--> TEST 01 : KO");
-				return false;
-			} else {
-				System.out.println("--> TEST 01 : OK");
-				return true;
-
-			}
-
+		System.out.println("=======" + "  Tests ArrayList  " + "=======\n");
+		passed = execTests(new ArrayList());
+		if (passed) {
+			nbTestsPassed++;
 		}
+		System.out.println("\n");
 
-		static boolean test02() {
-			boolean passed = true;
-
-			IList list = new ArrayList();
-			list.insertAt(0, "Hello");
-			list.insertAt(1, "World");
-			if (!list.elementAt(0).equals("Hello")) {
-				System.out.println("Wrong element at index 0 !\n");
-				passed = false;
-			}
-			if (!list.elementAt(1).equals("World")) {
-				System.out.println("Wrong element at index 1 !\n");
-				passed = false;
-			}
-			list.updateAt(1, "everyone");
-			if (!list.elementAt(1).equals("everyone")) {
-				System.out.println("Wrong element at index 1 !\n");
-				passed = false;
-			}
-			list.insertAt(4, "coucou");
-			if (!list.elementAt(4).equals("coucou") || list.length() != 5) {
-				System.out.println("Wrong element at index 10 !\n");
-				passed = false;
-			}
-			
-			iteratorTest(list);
-			
-			if (!passed) {
-				System.out.println("--> TEST 02 : KO");
-				return false;
-			} else {
-				System.out.println("--> TEST 02 : OK");
-				return true;
-
-			}
-		}
-
-		static boolean test03() {
-			boolean passed = true;
-			IList list = new ArrayList();
-			for (int i = 0; i <= 15; i++) {
-				list.insertAt(i, i);
-
-				// enlève les nombres pairs
-			}
-			for (int j = 0; j < list.length(); j++) {
-				list.removeAt(j % list.length());
-			}
-			for (int i = 0; i < list.length(); i++) {
-				if ((int) list.elementAt(i) % 2 == 0) {
-					passed = false;
-					break;
-				}
-			}
-
-			list.remove(1);
-			list.remove(3);
-			list.remove(9);
-			if ((list.contains(1) || list.contains(3) || list.contains(9)) || (list.length() != 5)) {
-				passed = false;
-			}
-			
-			iteratorTest(list);
-
-			if (!passed) {
-				System.out.println("--> TEST 03 : KO");
-				return false;
-			} else {
-				System.out.println("--> TEST 03 : OK");
-				return true;
-
-			}
-		}
-
-		static boolean test04() {
-			boolean passed = true;
-			IList list = new ArrayList();
-			Object[] objs = {"A","B","C","D"};
-			list.toArray(objs);
-			for(int i = 0 ; i < objs.length ; i++) {
-				if(!list.elementAt(i).equals(objs[i]) || list.length() != 4) {
-					System.out.println("Error\n");
-					passed = false;
-				}
-			}
-			
-			iteratorTest(list);
-			if (!passed) {
-				System.out.println("--> TEST 04 : KO");
-				return false;
-			} else {
-				System.out.println("--> TEST 04 : OK");
-				return true;
-
-			}
-		}
 		
-		static void iteratorTest(IList list) {
-			IList.Iterator it = ((ArrayList)list).iterator();
-			System.out.print("[ ");
+		// TESTS LinkedList
+		System.out.println("=======" + "  Tests LinkedList  " + "=======\n");
+		passed = execTests(new LinkedList());
+		if (passed) {
+			nbTestsPassed++;
+		}
+		System.out.println("\n");
 
-			while (it.hasNext()) {
-				System.out.print(it.next());
-				if (it.hasNext()) {
-					System.out.print(", ");
-
-				}
-			}
-			System.out.print(" ] ");
+		// All Tests passed ? ;)
+		if (passed && nbTestsPassed == 2) {
+			System.out.println("==> All Tests : PASSED");
+		} else {
+			System.out.println("==> All Tests : FAILED");
 		}
 
-		static void execTests() {
-			System.out.println("=======" + "  Tests ArrayList  " + "=======\n");
-			if(test01() && test02() && test03() && test04()) {
-				System.out.println("\nTests ArrayList : PASSED");
-			}else {
-				System.out.println("\nTests ArrayList : FAILED");
-				System.exit(-1);
-			}
+	}
+
+	static void initList(IList list) {
+		while(list.length() > 0) {
+			list.removeAt(0);
 		}
 	}
+
+	static boolean test01(IList list) {
+		boolean passed = true;
+		initList(list);
+		Object[] m_obj = { 1, 2, 3, 4, 5 };
+		list.toArray(m_obj);
+		if (list.length() != 5) {
+			System.out.println("Wrong size ! \n");
+			passed = false;
+		}
+		for (int i = 0; i < list.length(); i++) {
+			if (!list.elementAt(i).equals(m_obj[i])) {
+				System.out.println("Wrong element ! \n");
+				passed = false;
+			}
+		}
+
+		iteratorTest(list);
+
+		if (!passed) {
+			System.out.println("--> TEST 01 : KO");
+			return false;
+		} else {
+			System.out.println("--> TEST 01 : OK");
+			return true;
+
+		}
+
+	}
+
+	static boolean test02(IList list) {
+		boolean passed = true;
+		initList(list);
+		list.insertAt(0, "Hello");
+		list.insertAt(1, "World");
+		if (!list.elementAt(0).equals("Hello")) {
+			System.out.println("Wrong element at index 0 !\n");
+			passed = false;
+		}
+		if (!list.elementAt(1).equals("World")) {
+			System.out.println("Wrong element at index 1 !\n");
+			passed = false;
+		}
+		list.updateAt(1, "everyone");
+		if (!list.elementAt(1).equals("everyone")) {
+			System.out.println("Wrong element at index 1 !\n");
+			passed = false;
+		}
+		list.insertAt(4, "coucou");
+		if (!list.elementAt(4).equals("coucou") || list.length() != 5) {
+			System.out.println("Wrong element at index 4 !\n");
+			passed = false;
+		}
+
+		iteratorTest(list);
+
+		if (!passed) {
+			System.out.println("--> TEST 02 : KO");
+			return false;
+		} else {
+			System.out.println("--> TEST 02 : OK");
+			return true;
+
+		}
+	}
+
+	static boolean test03(IList list) {
+
+		boolean passed = true;
+		initList(list);
+
+		for (int i = 0; i <= 15; i++) {
+			list.insertAt(i, i);
+
+		}
+		// enlève les nombres pairs
+		for (int j = 0; j < list.length() ; j++) {
+			if ((int) list.elementAt(j) % 2 == 0) {
+				list.removeAt(j);
+			}
+		}
+		for (int i = 0; i < list.length(); i++) {
+			if ((int) list.elementAt(i) % 2 == 0) {
+				passed = false;
+				break;
+			}
+		}
+
+		list.remove(1);
+		list.remove(3);
+		list.remove(9);
+		if ((list.contains(1) || list.contains(3) || list.contains(9)) || (list.length() != 5)) {
+			passed = false;
+		}
+
+		iteratorTest(list);
+
+		if (!passed) {
+			System.out.println("--> TEST 03 : KO");
+			return false;
+		} else {
+			System.out.println("--> TEST 03 : OK");
+			return true;
+
+		}
+	}
+
+	static boolean test04(IList list) {
+		boolean passed = true;
+		initList(list);
+
+		Object[] objs = { "A", "B", "C", "D" };
+		list.toArray(objs);
+		for (int i = 0; i < objs.length; i++) {
+			if (!list.elementAt(i).equals(objs[i])) {
+				System.out.println("Error\n");
+				passed = false;
+			}
+		}
+
+		iteratorTest(list);
+		if (!passed) {
+			System.out.println("--> TEST 04 : KO");
+			return false;
+		} else {
+			System.out.println("--> TEST 04 : OK");
+			return true;
+
+		}
+	}
+
+	static void iteratorTest(IList list) {
+		ICollection.Iterator it = list.iterator();
+		System.out.print("[ ");
+
+		while (it.hasNext()) {
+			System.out.print(it.next());
+			if (it.hasNext()) {
+				System.out.print(", ");
+
+			}
+		}
+		System.out.print(" ] ");
+	}
+
+	static boolean execTests(IList list) {
+		if (test01(list) && test02(list) && test03(list) && test04(list)) {
+			System.out.println("\nTests : PASSED");
+			return true;
+		} else {
+			System.out.println("\nTests : FAILED");
+			return false;
+		}
+	}
+
 }
